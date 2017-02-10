9bcbe633b1a20ee1416b27bf4a10fa48cd0ce46d
peks-akinyede <Olanipekun.Akinyede@UCDConnect.ie>
2017-02-10 16:54:32 +0000

    Add files via upload

---
+++ Token.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import javax.swing.JComponent;
+
+public class Token extends JComponent{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int lenght;
+	private int breadth;
+	private int x;
+	private int y;
+	private Shape shape;
+	private Color color;
+	public Token(){ 
+		super();
+		setVisible(true);
+		this.lenght = 15;
+		this.breadth =15;
+		this.x = 5;
+		this.y = 5;
+		this.shape = new Rectangle(this.x,this.y, this.lenght, this.breadth);
+		this.color = Color.BLACK;
+		
+	}
+	
+	public Token(Color color){
+		this();
+		this.color = color;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	
+	public void setPosition(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public void paintComponent(Graphics g) {
+	 	 Graphics2D g2 = (Graphics2D) g;
+	 	super.paintComponent(g);
+	 	
+	 	g2.setColor(color);
+	 	g2.fill(shape);
+	 
+	 	 g2.draw(this.shape);
+	}
+	
+	public Shape getShape() {
+		// TODO Auto-generated method stub
+		return this.shape;
+	}
+}
