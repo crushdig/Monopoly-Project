a947506ad2ed010a9ae104241472ea6e9cf59bc4
crushdig <boluwade.alabi@ucdconnect.ie>
2017-02-12 17:11:26 +0000

    Update UI_Monopoly_Board.java

--- UI_Monopoly_Board.java
+++ UI_Monopoly_Board.java
@@ -1,94 +1,264 @@
 package sprint_One;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 
-public class UI_Monopoly_Board
-{	
-	//gets the path for particular file of choice
-	private static String image = "C:\\Users\\bolu_\\workspace\\Java\\Software Engineering\\sprint_One\\Monopoly_board.jpg";
-	//Creates a file object for the file that's to be opened
-	private static File inputImage = new File(image);
-	private static BufferedImage load_Image_Data; 
-	private static JLabel imageLabel;
-	private static JFrame imageFrame = new JFrame();
-	private static JPanel panel = new JPanel();
-	final static int field_Width = 60;
+public class UI_Monopoly_Board extends JFrame
+{
+
+	Point[] locations = { new Point(630,643), new Point(570,643),new Point(510,643), 
+			new Point(450,643),new Point(390,643), new Point(330,643),new Point(270,643), new Point(210,643),
+			new Point(150,643), new Point(95,643),new Point(60,643), new Point(60,573),
+			new Point(60,503), new Point(60,433),new Point(60,383), new Point(60,323),
+			new Point(60,273), new Point(60,213),new Point(60,153), new Point(60,93),new Point(60,33), 
+			
+			new Point(120,13),new Point(180,13), new Point(230,13),new Point(280,13), 
+			new Point(340,13),new Point(400,13), new Point(460,13),new Point(520,13), new Point(580,13)
+			,new Point(660,60),
+			new Point(660,120),
+			new Point(660,160), new Point(660,220),new Point(660,280), new Point(660,340),new Point(660,400),
+			new Point(660,460),new Point(660,520), new Point(660,580), new Point(660,640)
+		};
+	
+	Point defaultPosition = new Point(600,603);
+	
+	private int players;
+	private Token[] token ;
+
+	private static JPanel infopanel;
+	private static JPanel commandpanel ;
+//	private static JPanel basepanel;
+
+	final static int field_Width = 20;
 	private static JTextField commandField = new JTextField(field_Width);
 	private static JLabel commandLabel = new JLabel("Enter Command: ");
-	private static JLabel resultLabel = new JLabel();
+	
 	private Border blacklineBorder;
 	private final int ROWS = 35;
-	private final int COLUMNS = 20;
+	private final int COLUMNS = 40;
 	private JTextArea textArea =  new JTextArea(ROWS, COLUMNS);
+	private static JLabel resultLabel = new JLabel();
+	private JLayeredPane lp = getLayeredPane();
 	
-	public UI_Monopoly_Board() throws IOException
-	{	
-		/* Loads all the properties of the image by making use of bufferedImages's colorModel. 
-		 * Image is located using an image reader and then read in as a file. */ 
-		load_Image_Data = ImageIO.read(inputImage); 
-		
-		/* Creating a JLable object which takes in an ImageIcon object that loads the image 
-		 * being used. The JLabel loads the image as a label in the JFrame. */
-		imageLabel = new JLabel(new ImageIcon(load_Image_Data));
-		
-		imageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		imageFrame.getContentPane().add(imageLabel);
-		imageFrame.setTitle("Welcome to Monopoly");
-		imageFrame.setSize(300, 400);
-	
-		JPanel panel1 = new JPanel();		
-		panel1.setBackground(Color.black);
-		panel1.add(imageLabel);
-		
-//		panel.setBackground(Color.black);
-		panel.setLayout(new BorderLayout());
-		panel.add(panel1, BorderLayout.CENTER);
-		
-		imageFrame.add(panel);
-		
-		imageFrame.pack();
-		imageFrame.setVisible(true);
-		
-	}
-	
-	public void InformationPanel()
+//	private static JLabel monopolyLabel;
+
+	public UI_Monopoly_Board()
 	{
-		JPanel panel2 = new JPanel();
+		players = 6;
+		token = new Token[players];
+
+		int offset = 10;
+
+		//Initialise tokens and spaces them out with offset
+
+		switch(players)
+		{
+			case 2:	
+				token[0] = new Token();
+				token[0].setBounds(10, 10, 700, 700);
+
+				token[0].setPosition(600,603);
+
+				token[1] = new Token(Color.red);
+			
+				token[1].setBounds(10, 10, 700, 700);
+
+				token[1].setPosition(600+offset,603+offset);
+
+				break;
+
+			case 3:	
+				token[0] = new Token();
+			
+				token[0].setBounds(10, 10, 700, 700);
+
+				token[0].setPosition(600,603);
+
+				token[1] = new Token(Color.red);
+			
+				token[1].setBounds(10, 10, 700, 700);
+
+				token[1].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+				
+				token[2] = new Token(Color.blue);
+			
+				token[2].setBounds(10, 10, 700, 700);
+
+				token[2].setPosition(600+offset,603+offset);
+
+				break;
+			
+			case 4:
+				token[0] = new Token();
+			
+				token[0].setBounds(10, 10, 700, 700);
+
+				token[0].setPosition(600,603);
+
+				token[1] = new Token(Color.red);
+			
+				token[1].setBounds(10, 10, 700, 700);
+
+				token[1].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+				
+				token[2] = new Token(Color.blue);
+			
+				token[2].setBounds(10, 10, 700, 700);
+
+				token[2].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[3] = new Token(Color.green);
+			
+				token[3].setBounds(10, 10, 700, 700);
+
+				token[3].setPosition(600+offset,603+offset);
+				break;
+			
+			case 5:
+				token[0] = new Token();
+			
+				token[0].setBounds(10, 10, 700, 700);
+
+				token[0].setPosition(600,603);
+
+				token[1] = new Token(Color.red);
+			
+				token[1].setBounds(10, 10, 700, 700);
+
+				token[1].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[2] = new Token(Color.blue);
+			
+				token[2].setBounds(10, 10, 700, 700);
+
+				token[2].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[3] = new Token(Color.green);
+			
+				token[3].setBounds(10, 10, 700, 700);
+
+				token[3].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[4] = new Token(Color.yellow);
+			
+				token[4].setBounds(10, 10, 700, 700);
+
+				token[4].setPosition(600+offset,603+offset);
+
+				break;
+			
+			case 6:
+				token[0] = new Token();
+			
+				token[0].setBounds(10, 10, 700, 700);
+
+				token[0].setPosition(600,603);
+
+				token[1] = new Token(Color.red);
+			
+				token[1].setBounds(10, 10, 700, 700);
+
+				token[1].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[2] = new Token(Color.blue);
+			
+				token[2].setBounds(10, 10, 700, 700);
+
+				token[2].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[3] = new Token(Color.green);
+				
+				token[3].setBounds(10, 10, 700, 700);
+
+				token[3].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[4] = new Token(Color.yellow);
+			
+				token[4].setBounds(10, 10, 700, 700);
+
+				token[4].setPosition(600+offset,603+offset);
+
+				offset = offset + 10;
+
+				token[5] = new Token(Color.cyan);
+				
+				token[5].setBounds(10, 10, 700, 700);
+
+				token[5].setPosition(600+offset,603+offset);
+
+				break;
+			
+			default:
+				System.out.println("Invalid number of players");
+		}
+
+
+		JLabel monopolyLabel = new JLabel(new ImageIcon("Monopoly_board.jpg"));
+		monopolyLabel.setBounds(-50, -30, 800, 750);
+
+		lp.add(monopolyLabel, new Integer(1));
+		lp.add(token[0], new Integer(2));
+		lp.add(token[1], new Integer(3));
+		lp.add(token[2], new Integer(4));
+		lp.add(token[3], new Integer(5));
+		lp.add(token[4], new Integer(6));
+		lp.add(token[5], new Integer(7));
+
+		
+		setSize(1500, 750);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setTitle("Welcome to Monopoly");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+		
+	public void information_Panel()
+	{
+		infopanel = new JPanel();
+		 
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		blacklineBorder = BorderFactory.createLineBorder(Color.BLACK);
 		TitledBorder title = BorderFactory.createTitledBorder(blacklineBorder, "Information Panel");
+		
 		scrollPane.getPreferredSize();
-		panel2.setBorder(title);
-		panel2.add(resultLabel, BorderLayout.NORTH);
+		infopanel.setBorder(title);
+		infopanel.add(resultLabel, BorderLayout.NORTH);
+		
 		textArea.setEditable(false);
-		panel2.add(scrollPane);
+		infopanel.add(scrollPane);
+		infopanel.setBounds(750, 0, 600, 600);
 		
-		
-		panel.add(panel2, BorderLayout.EAST);
+		lp.add(infopanel);
 	}
+		
 	
-	public void commandPanel_With_ActionPerformed()
-	{	
+	public void command_Panel()
+	{
+		commandpanel = new JPanel();
+		
 		blacklineBorder = BorderFactory.createLineBorder(Color.BLACK);
 		
 		JButton button = new JButton("Enter");
@@ -96,28 +266,70 @@ public class UI_Monopoly_Board
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-				String command = commandField.getText();
-				textArea.append(command + "\n");
-			}
-			
+				if(commandField.getText().isEmpty())
+				{
+					
+				}
+				
+				else
+				{
+					String command = commandField.getText();
+					textArea.append(command + "\n");
+				}
+			}			
 		} );
-
-		JPanel panel3 = new JPanel();
-		panel3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		button.setPreferredSize(new Dimension(65,20));
-		panel3.add(commandLabel);
-		panel3.add(commandField);
-		panel3.add(button);
 		
-		panel.add(panel3, BorderLayout.SOUTH);
+		getRootPane().setDefaultButton(button);
+		
+
+					
+		commandpanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		button.setPreferredSize(new Dimension(65,20));
+		commandpanel.add(commandLabel);
+		commandpanel.add(commandField);
+		commandpanel.add(button);
+		commandpanel.setBounds(750, 650, 500, 50);
+					
+//					basepanel = new JPanel();
+//					basepanel.setLayout(new BorderLayout());
+//					basepanel.add(infopanel,BorderLayout.EAST);
+//					basepanel.add(commandpanel,BorderLayout.SOUTH);
+//					basepanel.setBounds(0, 0, 1500, 1500);
+		
+		lp.add(commandpanel);
+
 	}
 	
-	public static void main(String[] args) throws IOException
-	{	
-		UI_Monopoly_Board obj = new UI_Monopoly_Board();
+	public void moveTokens() throws InterruptedException
+	{
 		
-		obj.InformationPanel();
-		obj.commandPanel_With_ActionPerformed();
+		int i, j, offset;
+		offset = 0;
+		
+		for(i = 0; i < token.length; i++)
+		{
+			for(j = 0; j < locations.length; j++)
+			{
+				token[i].setPosition(locations[j].x, locations[j].y);
+				repaint();
+				Thread.sleep(150);
+			}
+			
+			token[i].setPosition(defaultPosition.x + offset, defaultPosition.y+offset);
+			offset = offset + 15;
+		}
+	}
+	
+
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		// TODO Auto-generated method stub
+		UI_Monopoly_Board obj = new UI_Monopoly_Board();
+		obj.information_Panel();
+		obj.command_Panel();
+		obj.moveTokens();
+		
 	}
 
 }
