3e4cb01c01629627bce6e00786b490e0cdaf1b2a
peks-akinyede <Olanipekun.Akinyede@UCDConnect.ie>
2017-03-26 18:55:52 +0100

    Update CommandPanel.java

--- CommandPanel.java
+++ CommandPanel.java
@@ -1,4 +1,3 @@
-package sprint_Three;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -11,66 +10,75 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
+//This class is for the commandPanel
 
 public class CommandPanel extends JPanel  {
-    
-    private static final long serialVersionUID = 1L;
-    private static final int FONT_SIZE = 14;
-    String command;
-    
-    private JTextField commandField = new JTextField(); 
-    private LinkedList<String> commandBuffer = new LinkedList<String>();
-    
-    CommandPanel () {
-        class AddActionListener implements ActionListener {
-               public void actionPerformed(ActionEvent event)   {
-                   if(commandField.getText().isEmpty())
-                   {
-                     commandField.setText("");
-                   }
-                   else
-                   {
-                     synchronized (commandBuffer) {
-                       commandBuffer.add(commandField.getText());
-                       commandField.setText("");
-                       commandBuffer.notify();
-                     }
-                   }
-               }
-           }
-        
-        ActionListener listener = new AddActionListener();
-        commandField.addActionListener(listener);
-        commandField.setFont(new Font("Times New Roman", Font.PLAIN, FONT_SIZE));
-        setLayout(new BorderLayout());
-        add(commandField, BorderLayout.CENTER);
+	
+	private static final long serialVersionUID = 1L;
+	private static final int FONT_SIZE = 14;
+	private String string;
+	
+	private JTextField commandField = new JTextField(); 
+	private LinkedList<String> commandBuffer = new LinkedList<String>();
+	
+	//Action listener for return key
+	CommandPanel () {
+		class AddActionListener implements ActionListener {
+			   public void actionPerformed(ActionEvent event)	{
+				   synchronized (commandBuffer) {
+					   commandBuffer.add(commandField.getText());
+					   commandField.setText("");
+					   commandBuffer.notify();
+				   }
+		           return;
+			   }
+		   }
+		
+		ActionListener listener = new AddActionListener();
+		commandField.addActionListener(listener);
+		commandField.setFont(new Font("Times New Roman", Font.PLAIN, FONT_SIZE));
+		setLayout(new BorderLayout());
+		add(commandField, BorderLayout.CENTER);
 
     setBorder(BorderFactory.createLineBorder(Color.BLACK));
    
     setBounds(800, 630, 500, 50); // specifies the desired coordinates of the panel
                                                // being added to the layered pane
-    }
-    
-    
-    
-    public String getCommand() {
-        synchronized (commandBuffer) {
-            while (commandBuffer.isEmpty()) {
-                try {
-                    commandBuffer.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            command = commandBuffer.pop();
-        }
-        
-        return command;
-    }
-    
-//    public String getString()
-//    {
-//      return command;
-//    }
-     
+	}
+	
+	
+	
+	public String getCommand() {
+		String command;
+		synchronized (commandBuffer) {
+			while (commandBuffer.isEmpty()) {
+				try {
+					commandBuffer.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			command = commandBuffer.pop();
+		}
+		return command;
+	}
+	
+	public void inputString() {
+		synchronized (commandBuffer) {
+			while (commandBuffer.isEmpty()) {
+				try {
+					commandBuffer.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			string = commandBuffer.pop();
+		}
+		return;
+	}
+	
+	public String getString() {
+		return string;
+	}
+	 
 }
