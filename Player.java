d09b60f7d96c352e045abe41f83a73b363492243
crushdig <boluwade.alabi@ucdconnect.ie>
2017-04-10 09:58:51 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,191 +1,208 @@
+package sprint_Four;
 
 import java.util.ArrayList;
 
-//This class is for the player and their characteristics
+// This class is for the player and their characteristics
 public class Player {
 
-	private String name;
-	private double balance;
-	boolean bankrupt = false;
-	Token token;
-	private int position;
-	ArrayList<Property> assets;//Stores the players assets
+  private String name;
+  private double balance;
+  boolean bankrupt = false;
+  boolean inJail = false;
+  Token token;
+  private int position;
+  ArrayList<Property> assets;// Stores the players assets
 
-	public Player(String name){
-		this.name = name;
-		this.balance = 1500;
-		this.position = 0;
-		this.assets = new ArrayList<Property>();
-		this.token = new Token();
-	}
+  public Player(String name) {
+    this.name = name;
+    this.balance = 1500;
+    this.position = 0;
+    this.assets = new ArrayList<Property>();
+    this.token = new Token();
+  }
 
-	Player (Player player) {   // copy constructor
-		this(player.getName());
-	}
-	
-	public void setToken(Token token){
-		this.token = token;
-	}
-	
-	public Token getToken(){
-		return this.token;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
-	
-	public boolean isBankrupt(){
-		if(this.getbalance()<0){
-			bankrupt = true;
-		}
-		return bankrupt;
-	}
-	
-	public void move(int position){
-		this.position = position;
-	}
-	
-	public void move(){
-		position++;
-		if((position)>=40){
-			position = position%40;
-		}
-	}
+  Player(Player player) { // copy constructor
+    this(player.getName());
+  }
 
-	public int getPosition(){
-		return position;
-	}
+  public void setToken(Token token) {
+    this.token = token;
+  }
+  
+  public void setJail()
+  {
+    inJail =  true;
+  }
+  
+  public boolean getJail()
+  {
+    return inJail;
+  }
 
-	public void buy(Property property){
-		balance = balance - property.getValue();
-		assets.add(property);
-		property.setOwned(this);
-	}
+  public Token getToken() {
+    return this.token;
+  }
 
-	public void getRent(double d){
-		balance = balance + d;
-	}
+  public String getName() {
+    return this.name;
+  }
 
-	public int payRent(int rent){
-		balance = balance - rent;
-		return rent;
-	}
+  public boolean isBankrupt() {
+    if (this.getbalance() < 0) {
+      bankrupt = true;
+    }
+    return bankrupt;
+  }
 
-	public Property getLatestProperty () {
-		return assets.get(assets.size()-1);
-	}
+  public void move(int position) {
+    this.position = position;
+  }
 
-	public String getBalance(){
-		return "Balance is\t" + this.balance;
-	}
+  public void move() {
+    position++;
+    if ((position) >= 40) {
+      position = position % 40;
+    }
+  }
 
-	public double getbalance(){
-		return this.balance;
-	}
+  public int getPosition() {
+    return position;
+  }
+  
+  public int payBail(int bail)
+  {
+    balance = balance - bail;
+    return bail;
+  }
+  
+  public void buy(Property property) {
+    balance = balance - property.getValue();
+    assets.add(property);
+    property.setOwned(this);
+  }
 
-	
-	public ArrayList<Property> getProperties () {
-		return assets;
-	}
+  public void getRent(double d) {
+    balance = balance + d;
+  }
 
-	//Function for calculating player worth at the end of the game
-	public double getWorth(){
-		int worth = 0;
-		
-		for (Property property: assets) {
-			worth = worth + property.getValue();
-		}
-		
-		return worth + this.balance;
-	}
+  public int payRent(int rent) {
+    balance = balance - rent;
+    return rent;
+  }
 
-	public boolean Monopoly(Site site)
-	{	
-		boolean ownsAll = true;
-		ColourGroup colourGroup = site.getColourGroup();
-		for (Site s : colourGroup.getMembers()) {
-			if (!s.owned() || (s.owned() && s.getOwner() != this))
-				ownsAll = false;
-		}
-		return ownsAll;
-		
-	}
-	
-	public int getNumStationsOwned () {
-		int numOwned = 0;
-		for (Property p : assets) {
-			if (p instanceof Station) {
-				numOwned++;
-			}
-		}
-		return numOwned;
-	}
-	
-	public int getNumUtilitiesOwned () {
-		int numOwned = 0;
-		for (Property p : assets) {
-			if (p instanceof Utility) {
-				numOwned++;
-			}
-		}
-		return numOwned;
-	}
+  public Property getLatestProperty() {
+    return assets.get(assets.size() - 1);
+  }
 
-	public void mortgage(Property property){
-		property.mortgage();
-		balance += property.getMortgageValue();
-	}
+  public String getBalance() {
+    return "Balance is\t" + this.balance;
+  }
 
-	public void redeem(Property property){
-		property.setNotMortgaged();;
-		balance -= property.getMortgageValue();
-	}
+  public double getbalance() {
+    return this.balance;
+  }
 
-	public boolean equals (String name) {
-		return this.name.toLowerCase() == name;
-	}
-	
-	public String toString(){
-		return this.name;
-	}
-	
-	public boolean allMortgaged(){
-		for (Property p : assets) {
-			if (!p.isMortgaged()) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public int numberOfHouses(){
-		int houses = 0;
-		for (Property p : assets) {
-			if (p instanceof Site) {
-				if(((Site) p).getNumBuildings()<=4)
-				houses += ((Site) p).getNumHouses();
-			}
-		}
-		return houses;
-	}
-	
-	public int numberOfHotels(){
-		int hotels = 0;
-		for (Property p : assets) {
-			if (p instanceof Site) {
-				hotels += ((Site) p).getNumHotels();
-			}
-		}
-		return hotels;
-	}
-	
-	//for the cheat
-	public void addProperty (Property property) {
-		property.setOwned(this);
-		assets.add(property);
-		return;
-	}
-	
+
+  public ArrayList<Property> getProperties() {
+    return assets;
+  }
+
+  // Function for calculating player worth at the end of the game
+  public double getWorth() {
+    int worth = 0;
+
+    for (Property property : assets) {
+      worth = worth + property.getValue();
+    }
+
+    return worth + this.balance;
+  }
+
+  public boolean Monopoly(Site site) {
+    boolean ownsAll = true;
+    ColourGroup colourGroup = site.getColourGroup();
+    for (Site s : colourGroup.getMembers()) {
+      if (!s.owned() || (s.owned() && s.getOwner() != this))
+        ownsAll = false;
+    }
+    return ownsAll;
+
+  }
+
+  public int getNumStationsOwned() {
+    int numOwned = 0;
+    for (Property p : assets) {
+      if (p instanceof Station) {
+        numOwned++;
+      }
+    }
+    return numOwned;
+  }
+
+  public int getNumUtilitiesOwned() {
+    int numOwned = 0;
+    for (Property p : assets) {
+      if (p instanceof Utility) {
+        numOwned++;
+      }
+    }
+    return numOwned;
+  }
+
+  public void mortgage(Property property) {
+    property.mortgage();
+    balance += property.getMortgageValue();
+  }
+
+  public void redeem(Property property) {
+    property.setNotMortgaged();;
+    balance -= property.getMortgageValue();
+  }
+
+  public boolean equals(String name) {
+    return this.name.toLowerCase() == name;
+  }
+
+  public String toString() {
+    return this.name;
+  }
+
+  public boolean allMortgaged() {
+    for (Property p : assets) {
+      if (!p.isMortgaged()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public int numberOfHouses() {
+    int houses = 0;
+    for (Property p : assets) {
+      if (p instanceof Site) {
+        if (((Site) p).getNumBuildings() <= 4)
+          houses += ((Site) p).getNumHouses();
+      }
+    }
+    return houses;
+  }
+
+  public int numberOfHotels() {
+    int hotels = 0;
+    for (Property p : assets) {
+      if (p instanceof Site) {
+        hotels += ((Site) p).getNumHotels();
+      }
+    }
+    return hotels;
+  }
+
+  // for the cheat
+  public void addProperty(Property property) {
+    property.setOwned(this);
+    assets.add(property);
+    return;
+  }
+
 
 }
